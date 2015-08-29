a089dc8253d1cb4a7cd9786e1e6deb6463309cf0
Chackle <chackle@gmail.com>
2015-08-29 16:06:52 +0100

    Started fleshing out Comment object.

--- PlayerAuthMe/Library/Models/Comment.swift
+++ PlayerAuthMe/Library/Models/Comment.swift
@@ -10,8 +10,41 @@ import Foundation
 
 class Comment {
   
+  var id: Int
+  var userId: Int
+  var activityUserId: Int
+  var activityId: Int
+  var createdAtString: String
+  var updatedAtString: String
+  var editedAtString: String?
+  var userIsBlocked: Bool
+  var isLiked: Bool
+  var url: String
+  var showDelete: Bool
+  var showEdit: Bool
+  var isOwnComment: Bool
+  var likesCount: Int
+  var text: String
+  var raw: String
+  var user: Player
+  
   // TODO: Flesh out and implement into feed GET
-  init() {
-    
+  init(id: Int, userId: Int, activityUserId: Int, activityId: Int, createdAtString: String, updatedAtString: String, userIsBlocked: Bool, isLiked: Bool, url: String, showDelete: Bool, showEdit: Bool, isOwnComment: Bool, likesCount: Int, text: String, raw: String, user: Player) {
+    self.id = id
+    self.userId = userId
+    self.activityUserId = activityUserId
+    self.activityId = activityId
+    self.createdAtString = createdAtString
+    self.updatedAtString = updatedAtString
+    self.userIsBlocked = userIsBlocked
+    self.isLiked = isLiked
+    self.url = url
+    self.showDelete = showDelete
+    self.showEdit = showEdit
+    self.isOwnComment = isOwnComment
+    self.likesCount = likesCount
+    self.text = text
+    self.raw = raw
+    self.user = user
   }
 }
